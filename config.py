e3878e3bcb2ee42fc7c17bdc10b96cf7ca476bb1
MohammadTaha1388 <mhmdtahaafdlygrwh2@gmail.com>
2026-06-25 17:04:56 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,3 @@
-# config.py
-
-import os
-
-# توکن ربات
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# حالت توسعه یا پروداکشن
-DEBUG = True
-
-# نام ربات
-BOT_NAME = "MasirNo"
-
-# پیام‌های ثابت
-WELCOME_TEXT = "👋 به مسیرنو خوش آمدی! هدف خودت را انتخاب کن:"
-
-SELECT_GOAL_TEXT = "🎯 یکی از اهداف زیر را انتخاب کن:"
-
-DONE_TEXT = "🔥 آفرین! امروز کارت انجام شد"
-
-FAIL_TEXT = "⚠️ مشکلی نیست، فردا دوباره شروع کن"
+BOT_TOKEN = "8342491323:AAFgmXGyHjNI086EucC1K5WDCKUHIMiuPG0"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
